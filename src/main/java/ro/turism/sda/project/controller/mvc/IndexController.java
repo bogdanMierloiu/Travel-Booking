0b53afe73903f6b4366bbfc8701748f9e179e2fd
Bogdan Mierloiu <bogdy.musiclover@gmail.com>
2023-01-17 22:23:33 +0200

    first destination with photo and book button

--- src/main/java/ro/turism/sda/project/controller/mvc/IndexController.java
+++ src/main/java/ro/turism/sda/project/controller/mvc/IndexController.java
@@ -24,7 +24,26 @@ public class IndexController {
     @GetMapping("")
     public String viewIndexPage(Model model) {
         List<DestinationResponse> destinationResponses = destinationService.getAll();
+        DestinationResponse amsterdam = destinationService.findById(1);
+        DestinationResponse athena = destinationService.findById(2);
+        DestinationResponse frankfurt = destinationService.findById(3);
+        DestinationResponse luxembourg = destinationService.findById(4);
+        DestinationResponse newYork = destinationService.findById(5);
+        DestinationResponse paris = destinationService.findById(6);
+        DestinationResponse romeColosseum = destinationService.findById(7);
+        DestinationResponse sweden = destinationService.findById(8);
+        DestinationResponse switzerland = destinationService.findById(9);
+        model.addAttribute("amsterdam", amsterdam);
         model.addAttribute("destinations", destinationResponses);
+        model.addAttribute("athena", athena);
+        model.addAttribute("frankfurt", frankfurt);
+        model.addAttribute("luxembourg", luxembourg);
+        model.addAttribute("newYork", newYork);
+        model.addAttribute("paris", paris);
+        model.addAttribute("romeColosseum", romeColosseum);
+        model.addAttribute("sweden", sweden);
+        model.addAttribute("switzerland", switzerland);
+
         return "index";
     }
 
